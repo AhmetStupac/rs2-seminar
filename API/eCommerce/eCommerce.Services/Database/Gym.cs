b41329dfb79d32522ed419168edae182f4bf4a2c
Ahmet Stupac <ahmetst81@hotmail.com>
2026-02-03 16:40:43 +0100

    changed gym entity

--- API/eCommerce/eCommerce.Services/Database/Gym.cs
+++ API/eCommerce/eCommerce.Services/Database/Gym.cs
@@ -13,7 +13,7 @@ namespace eCommerce.Services.Database
         public string Name { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
-        public string Drzava { get; set; }
+        public string Country { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string WorkTime { get; set; }
